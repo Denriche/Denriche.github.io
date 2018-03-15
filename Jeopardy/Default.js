64e13bb4c6ea74dd8b6fe3ad333286f6b06c646c
Denriche <denrichetan12@gmail.com>
2018-03-15 21:25:44 +0800

    Add files via upload

--- Jeopardy/Default.js
+++ Jeopardy/Default.js
@@ -2,6 +2,7 @@ var xhr = new XHLHttpRequest()
 
 xhr addEventListener('load', getCategories() {
 	if(xhr.status == 200) {
+
 		console.log(xhr.response)
 	} else {
 		console.log(xhr.statusText)
@@ -12,12 +13,15 @@ xhr addEventListener('error', function() {
 	console.log(Error('Network Error'))
 })
 
-xhr.open('GET', https://jservice.io/api/categories?count=5&offset=10)
+xhr.open('GET', "https://jservice.io/api/categories?count=5&offset=10")
 xhr.send()
+
+var categories = document.getElementId("test");
 
 function getCategories() {
 	JSON.parse(xhr.response)
 }
+
 function getQuestion(id) {
 	'jservice...'+id
 }
